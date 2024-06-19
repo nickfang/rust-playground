2bede4cf4c9a2f7fbb05049c0a21c41bd04ce299
Nick Fang <fang.nicholas@gmail.com>
2024-06-19 10:11:16 -0500

    Going through intro and variables exercies.

--- rustlings/exercises/01_variables/variables6.rs
+++ rustlings/exercises/01_variables/variables6.rs
@@ -3,9 +3,8 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// consts must be explicitly typed
+const NUMBER: i64 = 3;
 fn main() {
-    println!("Number {}", NUMBER);
+  println!("Number {}", NUMBER);
 }
