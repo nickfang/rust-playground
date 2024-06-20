3a4cfe0a998ec396548da535b884a1a16cc7531b
Nick Fang <fang.nicholas@gmail.com>
2024-06-20 17:11:20 -0500

    Going through more of rustlings.

--- rustlings/exercises/04_primitive_types/primitive_types3.rs
+++ rustlings/exercises/04_primitive_types/primitive_types3.rs
@@ -8,12 +8,12 @@
 // I AM NOT DONE
 
 fn main() {
-    let a = ???
+  let a = ['a'; 100];
 
-    if a.len() >= 100 {
-        println!("Wow, that's a big array!");
-    } else {
-        println!("Meh, I eat arrays like that for breakfast.");
-        panic!("Array not big enough, more elements needed")
-    }
+  if a.len() >= 100 {
+    println!("Wow, that's a big array!");
+  } else {
+    println!("Meh, I eat arrays like that for breakfast.");
+    panic!("Array not big enough, more elements needed")
+  }
 }
