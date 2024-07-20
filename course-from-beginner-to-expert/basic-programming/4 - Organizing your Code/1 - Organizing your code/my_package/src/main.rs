d93f759b7bfeaac721fa2d3692bf12adbeca9d32
Nick Fang <fang.nicholas@gmail.com>
2024-07-20 12:30:28 -0500

    Working with crates and modules.

--- course-from-beginner-to-expert/basic-programming/4 - Organizing your Code/1 - Organizing your code/my_package/src/main.rs
+++ course-from-beginner-to-expert/basic-programming/4 - Organizing your Code/1 - Organizing your code/my_package/src/main.rs
@@ -1,3 +1,16 @@
+use my_package::{ product::category::Category, customer::Customer, order::Order, product::Product };
+// use my_package::customer::Customer;
+// use my_package::product::Product;
+// use my_package::product::category::Category;
+// use my_package::order::Order;
+
 fn main() {
-    println!("Hello, world!");
+  let product: Product = Product::new(1, "Laptop".to_string(), 1000.0, Category::Electronics);
+  let customer: Customer = Customer::new(
+    1,
+    String::from("Alice"),
+    String::from("alic@example.com")
+  );
+  let order: Order = Order::new(1, product, customer, 2);
+  println!("Total cost of the order: ${}", order.total_bill());
 }
---
+++ course-from-beginner-to-expert/basic-programming/4 - Organizing your Code/1 - Organizing your code/my_package/src/product.rs
@@ -0,0 +1,25 @@
+use category::Category;
+
+pub struct Product {
+  id: u64,
+  name: String,
+  price: f64,
+  category: Category,
+}
+impl Product {
+  pub fn new(id: u64, name: String, price: f64, category: Category) -> Product {
+    Product { id, name, price, category }
+  }
+}
+
+pub mod category;
+
+impl Product {
+  fn calculate_tax(&self) -> f64 {
+    self.price * 0.1
+  }
+
+  pub fn product_price(&self) -> f64 {
+    self.price + self.calculate_tax()
+  }
+}
